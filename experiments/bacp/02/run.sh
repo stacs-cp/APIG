bf345793796b891847e34bca2551dee844b6e6a2
ndangtt <ndangtt@gmail.com>
2021-05-04 16:41:05 +0100

    update path

--- experiments/bacp/02/run.sh
+++ experiments/bacp/02/run.sh
@@ -1,10 +1,10 @@
 rm -rf detailed-output
 
 source ~/APIG/scripts/set-path.sh
-pushd /home/nttd/APIG/bin/irace-source; ./install.sh; popd
+pushd ~/APIG/bin/irace-source; ./install.sh; popd
 
 mkdir -p detailed-output/
 cp problem.eprime generator.eprime repair.eprime detailed-output/
 
-irace --seed 123 --scenario /home/nttd/APIG/scripts/tuning-files/scenario.R --parameter-file params.irace --train-instances-file instances --exec-dir ./ --max-experiments 504 --target-runner /home/nttd/APIG/scripts/tuning-files/target-runner --parallel 2
+irace --seed 123 --scenario ~/APIG/scripts/tuning-files/scenario.R --parameter-file params.irace --train-instances-file instances --exec-dir ./ --max-experiments 504 --target-runner ~/APIG/scripts/tuning-files/target-runner --parallel 2
 
